7eca5b581e021765e2b125dc54c14cf0549114b3
55819986 <olrouqc42@gmail.com>
2020-02-06 20:43:06 -0500

    added Tensorboard support, to be tested

--- src/model/conv2d.py
+++ src/model/conv2d.py
@@ -1,11 +1,12 @@
 import logging
 
+import tensorflow as tf
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import (Activation, Conv2D, Dense, Flatten,
-                                     MaxPooling2D)
+from tensorflow.keras.layers import Activation, Conv2D, Dense, Flatten, MaxPooling2D
 from tensorflow.keras.optimizers import SGD
 
 from src.data.train import load_data
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
@@ -37,10 +38,16 @@ def train(model, batch_size=32):
     optimizer = SGD(0.0001)
     model.compile(loss="mean_squared_error", optimizer=optimizer, metrics=["mse"])
 
+    log_directory = "/project/cq-training-1/project1/teams/team10/result_log"
+    +datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = tf.keras.callbacks.Tensorboard(
+        log_dir=log_directory, histogram_freq=1
+    )
+
     historic = model.fit_generator(
         train_set.batch(batch_size),
         validation_data=valid_set.batch(batch_size),
+        callbacks=[tensorboard_callback],
         epochs=1,
     )
     logger.info("Done.")
-    print(historic)
