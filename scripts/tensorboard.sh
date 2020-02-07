e13784ba838215a303f52e3a022a6e1bae5b6541
nathaniel <nathaniel.simard.42@gmail.com>
2020-02-07 22:12:18 +0000

    Fix script

--- scripts/tensorboard.sh
+++ scripts/tensorboard.sh
@@ -3,7 +3,7 @@
 file=$1
 port=$2
 
-tensorboard --logdir /project/cq-training-1/project1/teams/team10/tensorboard/$file \
+tensorboard --logdir $file \
             --port $port \
             --host localhost \
             --reload_interval 5
