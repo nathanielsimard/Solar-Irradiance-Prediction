eeb46981bce20bd15445146c4a68bc8bcf55dabd
55819986 <olivier10_10@hotmail.com>
2020-02-09 23:44:42 -0500

    Made the training loop more flexible. To be tested.

--- src/data/train.py
+++ src/data/train.py
@@ -6,7 +6,6 @@ from datetime import datetime
 import tensorflow as tf
 
 # from tensorflow.keras import optimizers, losses
-from tensorflow.keras.callbacks import TensorBoard
 
 from src import logging
 from src.data import dataloader, split, preprocessing
@@ -24,16 +23,50 @@ STATION_COORDINATES = {
     Station.SXF: Coordinates(43.73403, -96.62328, 473),
 }
 
+CURRENT_TIME = datetime.now().strftime("%Y-%m-%d_%Hh%Mm%Ss")
+TRAIN_LOG_DIR = (
+    "/project/cq-training-1/project1/teams/team10/tensorboard/run-"
+    + CURRENT_TIME
+    + "/train"
+)
+VALID_LOG_DIR = (
+    "/project/cq-training-1/project1/teams/team10/tensorboard/run-"
+    + CURRENT_TIME
+    + "/valid"
+)
+MODEL_SAVE_DIR = "/project/cq-training-1/project1/teams/team10/models"
+CHECKPOINT_TIMESTAMP = 5
+
 
 class Training:
-    def __init__(self, optimizer, model: tf.keras.Model, loss_fn: str):
+    """Training class."""
+
+    def __init__(
+        self,
+        optimizer: tf.keras.optimizers,  # an optimizer object
+        model: tf.keras.Model,  # a model object, should have a str method and a call method
+        loss_fn: tf.keras.losses,  # a loss object
+    ):
+        """Initialize a training session."""
         self.optim = optimizer
         self.loss_fn = loss_fn
         self.model = model
+        self.train_loss = tf.keras.metrics.Mean("train loss", dtype=tf.float32)
+        self.valid_loss = tf.keras.metrics.Mean("valid loss", dtype=tf.float32)
 
-    def train(self, batch_size=128, epochs=10):
+        logger.info(model.summary())
+
+    def train(self, batch_size=128, epochs=10, valid_batch_size=256, caching=False):
+        """Performs the training of the model in minibatch.
+
+        Params:
+        A batch_size parameter that determines the minibatch size
+        A number of epochs
+        A valid_batch_size. Should be as large as the GPU can handle.
+        A boolean caching variable if temporary caching is desired.
+        """
         logger.info("Training" + str(self.model) + "model.")
-        train_set, valid_set, _ = load_data(enable_tf_caching=False)
+        train_set, valid_set, _ = load_data(enable_tf_caching=caching)
 
         scaling_image = preprocessing.MinMaxScaling(
             preprocessing.IMAGE_MIN, preprocessing.IMAGE_MAX
@@ -45,25 +78,66 @@ class Training:
         train_set = _scale_dataset(scaling_image, scaling_target, train_set)
         logger.info("Scaling valid set.")
         valid_set = _scale_dataset(scaling_image, scaling_target, valid_set)
-        logger.info("Compiling model.")
-        self.model.compile(
-            loss=self.loss, optimizer=self.optim, metrics=[self.loss],
-        )
-        log_directory = "/project/cq-training-1/project1/teams/team10/tensorboard/run-" + datetime.now().strftime(
-            "%Y-%m-%d_%Hh%Mm%Ss"
-        )
-        tensorboard_callback = TensorBoard(
-            log_dir=log_directory, update_freq="epoch", profile_batch=0
-        )
-        logger.info("Fit model.")
 
-        self.model.fit_generator(
-            train_set.batch(batch_size),
-            validation_data=valid_set.batch(batch_size),
-            callbacks=[tensorboard_callback],
-            epochs=epochs,
-        )
+        logger.info("Creating loss logs")
+        train_summary_writer = tf.summary.create_file_writer(TRAIN_LOG_DIR)
+        valid_summary_writer = tf.summary.create_file_writer(VALID_LOG_DIR)
+
+        logger.info("Fitting model.")
+        for epoch in range(epochs):
+            i = 0
+            logger.info("Training...")
+            for inputs, targets in train_set.batch(batch_size):
+                self._train_step(inputs, targets, training=True)
+                i += 1
+                print("Batch #", i)
+            with train_summary_writer.as_default():
+                tf.summary.scalar("train loss", self.train_loss.result(), step=epoch)
+
+            logger.info("Evaluating validation loss")
+            for inputs, targets in valid_set.batch(valid_batch_size):
+                self._valid_step(inputs, targets, training=False)
+            with valid_summary_writer.as_default():
+                tf.summary.scalar("valid loss", self.valid_loss.result(), step=epoch)
+
+            # From the Tensorflow documentation
+            template = (
+                "Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}"
+            )
+            print(
+                template.format(
+                    epoch + 1, self.train_loss.result(), self.valid_loss.result(),
+                )
+            )
+            # Reset the cumulative metrics after each epoch
+            self.train_loss.reset_states()
+            self.valid_loss.reset_states()
+            if epoch % CHECKPOINT_TIMESTAMP == 0:
+                logger.info("Checkpointing...")
+                self.model.save(
+                    filepath=MODEL_SAVE_DIR + str(self.model),
+                    save_format="tf",
+                    overwrite=True,
+                )
+
         logger.info("Done.")
+
+        @tf.function
+        def _train_step(self, train_inputs, train_targets, training: bool):
+            with tf.GradientTape() as tape:
+                outputs = self.model(train_inputs, training)
+                loss = self.loss_fn(train_targets, outputs)
+            gradients = tape.gradient(loss, self.model.trainable_variables)
+            self.optim.apply_gradients(zip(gradients, self.model.trainable_variables))
+
+            self.train_loss(loss)
+
+        @tf.function
+        def _valid_step(self, valid_inputs, valid_targets, training: bool):
+            outputs = self.model(valid_inputs, training)
+            loss = self.loss_fn(valid_targets, outputs)
+
+            self.valid_loss(loss)
 
 
 def default_cache_dir():
--- src/model/conv2d.py
+++ src/model/conv2d.py
@@ -1,30 +1,52 @@
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Activation, Conv2D, Dense, Flatten, MaxPooling2D
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Conv2D, Dense, Flatten, MaxPooling2D
+
+# from tensorflow.keras.activations import relu
 
 from src import logging
 
 logger = logging.create_logger(__name__)
 
 
-def create_model():
+class CNN2D(Model):
     """Create Conv2D model."""
-    input_shape = (64, 64, 5)
-    model = Sequential(
-        [
-            Conv2D(64, kernel_size=(5, 5), input_shape=input_shape),
-            Activation("relu"),
-            MaxPooling2D(pool_size=(2, 2)),
-            Conv2D(128, kernel_size=(5, 5)),
-            Activation("relu"),
-            MaxPooling2D(pool_size=(2, 2)),
-            Conv2D(128, kernel_size=(3, 3)),
-            Activation("relu"),
-            MaxPooling2D(pool_size=(2, 2)),
-            Flatten(),
-            Dense(256),
-            Activation("relu"),
-            Dense(4),
-        ]
-    )
-    logger.info(model.summary())
-    return model
+
+    def __init__(self):
+        """Initialize the architecture."""
+        super(CNN2D, self).__init__()
+        input_shape = (64, 64, 5)
+        self.conv1 = Conv2D(
+            64, kernel_size=(5, 5), input_shape=input_shape, activation="relu"
+        )
+        self.mp1 = MaxPooling2D(
+            pool_size=(2, 2)
+        )  # not sure if it goes there, it does not in PyTorch...
+
+        self.conv2 = Conv2D(128, kernel_size=(5, 5), activation="relu")
+        self.mp2 = MaxPooling2D(pool_size=(2, 2))
+
+        self.conv3 = Conv2D(128, kernel_size=(3, 3), activation="relu")
+        self.mp3 = MaxPooling2D(pool_size=(2, 2))
+
+        self.flatten = Flatten()
+        self.d1 = Dense(256, activation="relu")
+        self.d2 = Dense(4)
+
+    def __str__(self):
+        """Name of the model."""
+        return "Conv2D"
+
+    def call(self, x, training: bool):
+        """Performs the forward pass in the neural network.
+
+        Can use a different pass with the optional training boolean if some operations need to be skipped at evaluation(e.g. Dropout)
+        """
+        x = self.conv1(x)
+        x = self.mp1(x)  # Same here...
+        x = self.conv2(x)
+        x = self.mp2(x)
+        x = self.conv3(x)
+        x = self.mp3(x)
+        x = self.flatten(x)
+        x = self.d1(x)
+        return self.d2(x)
