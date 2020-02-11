0419b1a92d23f0bf7a955657f302873e67cab81a
nathaniel <nathaniel.simard.42@gmail.com>
2020-02-10 20:10:52 -0500

    Add more perf tests

--- perf/metadata.py
+++ perf/metadata.py
@@ -1,6 +1,8 @@
 import timeit
 
+from src.data import dataloader
 from src.data.metadata import Coordinates, MetadataLoader, Station
+from src.data.train import default_config
 
 CATALOG_PATH = "tests/data/samples/catalog-test.pkl"
 COORDINATES = Coordinates(40.05192, -88.37309, 230)
@@ -13,20 +15,46 @@ class MetadataPerf(object):
 
     def run(self):
         metadata = self.loader.load(STATION, COORDINATES, skip_missing=False)
-        i = 0
-        for m in metadata:
-            i += 1
-        print(i)
+        for i, m in enumerate(metadata):
+            if i % 100 == 0:
+                print(f"Loaded {i} metadata")
 
 
-def run():
+class DatasetPerf(object):
+    def __init__(self):
+        loader = MetadataLoader(CATALOG_PATH)
+
+        config = default_config()
+        config.error_strategy = dataloader.ErrorStrategy.ignore
+        config.features = [dataloader.Feature.target_ghi]
+
+        self.dataset = dataloader.create_dataset(
+            lambda: loader.load(STATION, COORDINATES, skip_missing=False), config=config
+        )
+
+    def run(self):
+        for i, m in enumerate(self.dataset):
+            if i % 100 == 0:
+                print(f"Loaded {i} targets")
+
+
+def run_metadataloader():
+    print("--- Metadata Loader Benchmark ---")
     perf_test = MetadataPerf()
-    print("--- Image Reder Benchmark ---")
     number = 10
     num_metadata = 2066
     result = timeit.timeit(perf_test.run, number=number)
-    print(f"Load metadata [{number*num_metadata/result:0.3f} medatada/s]\n")
+    print(f"Iterate over metadata [{number*num_metadata/result:0.3f} medatada/s]\n")
+
+
+def run_dataset():
+    print("--- Dataset Target Only Benchmark ---")
+    dataset_perf = DatasetPerf()
+    number = 10
+    num_target = 2066
+    result = timeit.timeit(dataset_perf.run, number=number)
+    print(f"Iterate over dataset [{number*num_target/result:0.3f} target/s]\n")
 
 
 if __name__ == "__main__":
-    run()
+    run_dataset()
