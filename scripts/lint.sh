bb6baaced732aee790db674d01178a9503139265
Raphael Jean <raphael.jean@rocketmail.com>
2020-02-12 16:03:10 -0500

    Check slurm tmpdir on CI

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh 
+echo $SLURM_TMPDIR
 echo flake8
 flake8 || exit 1
-echo pydocstyle
-pydocstyle src || exit 1
 echo black
 black . || exit 1
+echo pydocstyle
+pydocstyle src || exit 1
 echo mypy
 mypy --ignore-missing-imports --package src --package tests || exit 1
