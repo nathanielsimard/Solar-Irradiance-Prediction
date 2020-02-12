f14d9a3370f80489ef700cc214e95cf6459720e3
Number One Hundred Fifty Guest <guest150@helios3.helios.calculquebec.ca>
2020-02-12 20:38:56 +0000

    Latest and greatest script

--- scripts/train_job_test.sh
+++ scripts/train_job_test.sh
@@ -5,17 +5,25 @@
 #SBATCH --mem=16000M
 
 module load python/3.7
-source /project/cq-training-1/project1/teams/team10/env/bin/activate
+#source /project/cq-training-1/project1/teams/team10/env/bin/activate
+source /home/guest150/project1/bin/activate
+echo python run_train.py --skip_non_cached
+python -V
+#python run_train.py --skip_non_cached
+ls
 echo cp /project/cq-training-1/project1/teams/team10/image_reader_cache.tar $SLURM_TMPDIR
 date
 cp /project/cq-training-1/project1/teams/team10/image_reader_cache.tar $SLURM_TMPDIR
-pushd
+cwd=$(pwd)
 date
 cd $SLURM_TMPDIR
 pwd
 echo tar -xf image_reader_cache.tar
 tar -xf image_reader_cache.tar
 date
-du -d 1 .
-popd
-#python run_train.py
+#du -d 1 .
+cd $cwd
+source /home/guest150/project1/bin/activate
+ls
+python run_train.py --skip_non_cached
+
