d9a4ab88c05a4b255aee7d358d56784ad243dc2b
Raphael Jean <raphael.jean@rocketmail.com>
2020-02-11 21:25:25 -0500

    allow skipping non cached images

--- run_train.py
+++ run_train.py
@@ -18,11 +18,21 @@ def main():
         help="No training, no tensorflow, just the generator",
         action="store_true",
     )
+    parser.add_argument(
+        "--skip_non_cached",
+        help="Skip images which are not already cached in the image reader",
+        action="store_true",
+    )
     args = parser.parse_args()
     env.run_local = args.run_local
     model = conv2d.create_model()
     conv2d.logger.setLevel(logging.DEBUG)
-    conv2d.train(model, enable_tf_caching=args.enable_tf_caching, dry_run=args.dry_run)
+    conv2d.train(
+        model,
+        enable_tf_caching=args.enable_tf_caching,
+        dry_run=args.dry_run,
+        skip_non_cached=args.skip_non_cached,
+    )
 
 
 if __name__ == "__main__":
