056ea3b818858901d19a58b5950fa71556387dcf
nathaniel <nathaniel.simard.42@gmail.com>
2020-02-13 03:08:37 +0000

    Add some images

--- src/visualization/image.py
+++ src/visualization/image.py
@@ -1,36 +1,45 @@
 from matplotlib import pyplot as plt
 
-from src import logging
+import os
 from src.data.dataloader import Feature
 from src.data.train import default_config, load_data
 
-logger = logging.create_logger(__name__)
 
-
-def plot3d(channel="ch1"):
+def plot3d(channel="ch6"):
     config = default_config()
     config.features = [Feature.image]
     config.channels = [channel]
     config.crop_size = (300, 300)
-    config.time_interval_min = 60
-    config.num_images = 7
+    config.time_interval_min = 30
+    config.num_images = 10
 
     train_dataset, _, _ = load_data(config=config)
-
     images = _first_image(train_dataset)
 
-    logger.info(images.shape)
-    for i, img in enumerate(images[0]):
-        logger.info(img.shape)
-
+    images_name = []
+    for i, image in enumerate(images[0]):
         plt.cla()
         plt.clf()
         plt.axis("off")
-        imgg = img[:, :, 0]
+        image2d = image[:, :, 0]
 
-        logger.info(imgg.shape)
-        plt.imshow(imgg, cmap="gray")
-        plt.savefig(f"image-3D/{i}.png")
+        plt.imshow(image2d, cmap="gray")
+        name = f"assets/{i}.png"
+        images_name.append(name)
+        plt.savefig(name)
+
+    _make_gif("assets/image-6.gif", images_name)
+
+def _make_gif(file_name, images):
+    cmd = ""
+    for image in images:
+        cmd += f" {image}"
+
+    cmd_create = f"convert {cmd} {file_name}"
+    cmd_remove = f"rm {cmd}"
+
+    os.system(cmd_create)
+    os.system(cmd_remove)
 
 
 def _first_image(dataset):
