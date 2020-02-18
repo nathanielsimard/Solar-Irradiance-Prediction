67c75e43def2873d1a9cd51658601c53d2986696
nathaniel <nathaniel.simard.42@gmail.com>
2020-02-18 11:49:15 -0500

    Big

--- src/visualization/autoencoder.py
+++ src/visualization/autoencoder.py
@@ -7,26 +7,34 @@ from src.data.train import load_data
 from src.model.autoencoder import Autoencoder
 
 
-def show_images(images: List[np.ndarray], output_size, scale=0.1):
+def show_images(
+    originals: List[np.ndarray], generated: List[np.ndarray], output_size, scale=0.1
+):
     """Show images in a grid."""
     plt.cla()
     plt.clf()
 
+    num_rows = len(originals)
+    num_col = 2
+
     figsize_x = int(output_size[0] * scale)
     figsize_y = int(output_size[1] * scale)
 
-    fig, axs = plt.subplots(nrows=len(images), figsize=(figsize_x, figsize_y))
-    for row, image in enumerate(images):
-        ax = axs[row]
-        image = images[row]
+    fig, axs = plt.subplots(
+        nrows=num_rows, ncols=num_col, figsize=(figsize_x, figsize_y)
+    )
+    for row, (original, gen) in enumerate(zip(originals, generated)):
+        ax_original = axs[row][0]
+        ax_gen = axs[row][1]
 
-        if row == 0:
-            ax.set_title("img")
+        ax_original.set_xticks([])
+        ax_original.set_yticks([])
 
-        ax.set_xticks([])
-        ax.set_yticks([])
+        ax_gen.set_xticks([])
+        ax_gen.set_yticks([])
 
-        ax.imshow(image, cmap="gray")
+        ax_original.imshow(original, cmap="gray")
+        ax_gen.imshow(gen, cmap="gray")
 
 
 def show_image():
@@ -37,35 +45,25 @@ def show_image():
 
     train_dataset, _, _ = load_data(config=config)
     images = _first_image(train_dataset)
+    images_pred = _predict_images(autoencoder, images)
 
-    image_tt = autoencoder.scaling_image.normalize(images)
-    print(image_tt.shape)
-    images_pred = autoencoder((image_tt), False)
-    print(images_pred.shape)
-    images_pred = autoencoder.scaling_image.original(images_pred)
-    print(images_pred.shape)
-
-
+    images_preds = []
+    images_originals = []
     for i, (image, image_pred) in enumerate(zip(images, images_pred)):
         num_channels = image.shape[-1]
         for n in range(num_channels):
-            plt.cla()
-            plt.clf()
-            plt.axis("off")
-            plt.tight_layout()
-            image2d = image[:, :, n]
-            plt.imshow(image2d, cmap="gray")
-            name = f"assets/{n}-autoencoder.png"
-            plt.savefig(name)
+            images_originals.append(image[:, :, n])
+            images_preds.append(image_pred[:, :, n])
 
-            plt.cla()
-            plt.clf()
-            plt.axis("off")
-            plt.tight_layout()
-            image2d_pred = image_pred[:, :, n]
-            plt.imshow(image2d_pred, cmap="gray")
-            name = f"assets/{n}-autoencoder-pred.png"
-            plt.savefig(name)
+    show_images(images_originals, images_preds, config.crop_size)
+    plt.savefig(f"assets/autoencoder.png")
+
+
+def _predict_images(autoencoder, images):
+    images_scaled = autoencoder.scaling_image.normalize(images)
+    images_scaled_pred = autoencoder((images_scaled), False)
+    return autoencoder.scaling_image.original(images_scaled_pred)
+
 
 def _first_image(dataset, index=3):
     for i, data in enumerate(dataset.batch(1)):
