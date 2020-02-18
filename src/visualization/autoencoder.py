7261f083217d535d2f1bf2890a41878acfa51679
nathaniel <nathaniel.simard.42@gmail.com>
2020-02-18 11:23:42 -0500

    other wip

--- src/visualization/autoencoder.py
+++ src/visualization/autoencoder.py
@@ -1,7 +1,32 @@
+from typing import List
+
+import numpy as np
 from matplotlib import pyplot as plt
 
 from src.data.train import load_data
 from src.model.autoencoder import Autoencoder
+
+
+def show_images(images: List[np.ndarray], output_size, scale=0.1):
+    """Show images in a grid."""
+    plt.cla()
+    plt.clf()
+
+    figsize_x = int(output_size[0] * scale)
+    figsize_y = int(output_size[1] * scale)
+
+    fig, axs = plt.subplots(nrows=len(images), figsize=(figsize_x, figsize_y))
+    for row, image in enumerate(images):
+        ax = axs[row]
+        image = images[row]
+
+        if row == 0:
+            ax.set_title("img")
+
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+        ax.imshow(image, cmap="gray")
 
 
 def show_image():
@@ -12,6 +37,7 @@ def show_image():
 
     train_dataset, _, _ = load_data(config=config)
     images = _first_image(train_dataset)
+    imgs = []
     for i, image in enumerate(images):
         plt.cla()
         plt.clf()
@@ -21,9 +47,11 @@ def show_image():
         num_channels = image.shape[-1]
         for n in range(num_channels):
             image2d = image[:, :, n]
+            imgs.append(image2d)
             plt.imshow(image2d, cmap="gray")
             name = f"assets/{n}-autoencoder.png"
             plt.savefig(name)
+    show_images([imgs], config.crop_size)
 
 
 def _first_image(dataset, index=3):
