4c546b6b027265b1d7cab1cdd1fb2cde34a10d2e
nathaniel <nathaniel.simard.42@gmail.com>
2020-02-20 15:27:18 -0500

    Add script

---
+++ scripts/evalulation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Setup the environment
+module load python/3.7
+source /project/cq-training-1/project1/teams/team10/env/bin/activate
+
+# Go to the right folder
+cd /project/cq-training-1/project1/teams/team10/evaluation
+
+# Run the evaluator with all the inputs
+python evaluator.py $@
+
