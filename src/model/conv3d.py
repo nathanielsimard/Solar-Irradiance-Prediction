cab088088e44194a521ba431150161fe17edf13b
nathaniel <nathaniel.simard.42@gmail.com>
2020-02-21 16:29:12 -0500

    Better stringer architecture

--- src/model/conv3d.py
+++ src/model/conv3d.py
@@ -1,7 +1,8 @@
 from typing import Tuple
 
 import tensorflow as tf
-from tensorflow.keras.layers import Conv3D, Dense, Flatten, MaxPooling3D
+from tensorflow.keras.layers import (Conv3D, Dense, Dropout, Flatten,
+                                     MaxPooling3D)
 from tensorflow.keras.models import Sequential
 
 from src import logging
@@ -17,19 +18,21 @@ NAME = "Conv3D"
 class CNN3D(base.Model):
     """Create Conv3D model."""
 
-    def __init__(self, num_images=6):
+    def __init__(self, num_images=6, crop_size=16, dropout=0.25):
         """Initialize the architecture."""
         super().__init__(NAME)
         self.scaling_image = preprocessing.MinMaxScaling(
             preprocessing.IMAGE_MIN, preprocessing.IMAGE_MAX
         )
         self.num_images = num_images
+        self.crop_size = crop_size
 
-        self.conv1 = self._convolution_step((1, 5, 5), 64)
-        self.conv2 = self._convolution_step((1, 3, 3), 128)
-        self.conv3 = self._convolution_step((1, 3, 3), 256)
+        self.conv1 = self._convolution_step((3, 3, 3), 32)
+        self.conv2 = self._convolution_step((3, 3, 3), 64)
+        self.conv3 = self._convolution_step((3, 3, 3), 128)
 
         self.flatten = Flatten()
+        self.dropout = Dropout(dropout)
 
         self.d1 = Dense(1024, activation="relu")
         self.d2 = Dense(512, activation="relu")
@@ -47,7 +50,7 @@ class CNN3D(base.Model):
         # (Batch, images, width, height, channels)
         image_size_x = images.shape[2]
         image_size_y = images.shape[3]
-        pixel = 12
+        pixel = self.crop_size
         start_x = image_size_x // 2 - pixel // 2
         end_x = image_size_x // 2 + pixel // 2
         start_y = image_size_y // 2 - pixel // 2
@@ -58,7 +61,15 @@ class CNN3D(base.Model):
         logger.info(f"Cropped images shape: {crop.shape}")
 
         x = self.conv1(crop)
+
+        if training:
+            x = self.dropout(x)
+
         x = self.conv2(x)
+
+        if training:
+            x = self.dropout(x)
+
         x = self.conv3(x)
 
         x = self.flatten(x)
@@ -66,19 +77,34 @@ class CNN3D(base.Model):
         x = tf.concat([x, clearsky], 1)
 
         x = self.d1(x)
+
+        if training:
+            x = self.dropout(x)
+
         x = self.d2(x)
+
+        if training:
+            x = self.dropout(x)
+
         x = self.d3(x)
+
+        if training:
+            x = self.dropout(x)
+
         x = self.d4(x)
 
         return x
 
     def _convolution_step(self, kernel_size, channels):
-        conv3d_1 = Conv3D(channels, kernel_size=kernel_size, activation="relu")
-        conv3d_2 = Conv3D(channels, kernel_size=kernel_size, activation="relu")
-        conv3d_3 = Conv3D(channels, kernel_size=kernel_size, activation="relu")
+        conv3d_1 = Conv3D(
+            channels, kernel_size=kernel_size, activation="relu", padding="same"
+        )
+        conv3d_2 = Conv3D(
+            channels, kernel_size=kernel_size, activation="relu", padding="same"
+        )
         max_pool = MaxPooling3D(pool_size=(1, 2, 2))
 
-        return Sequential([conv3d_1, conv3d_2, conv3d_3, max_pool])
+        return Sequential([conv3d_1, conv3d_2, max_pool])
 
     def config(self, training=False) -> dataloader.DataloaderConfig:
         """Configuration."""
