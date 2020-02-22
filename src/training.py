d9be07a270e668f7ee23e8c5ac054a2f66d7029f
Tryong <olivier10_10@hotmail.com>
2020-02-22 17:22:39 -0500

    test

--- src/training.py
+++ src/training.py
@@ -117,7 +117,7 @@ class Training(object):
         )
 
         logger.info("Apply Preprocessing")
-        train_set = self.model.preprocess(train_set).cache("aa-train")
+        train_set = self.model.preprocess(train_set, training=True).cache("aa-train")
         valid_set = self.model.preprocess(valid_set, training=False).cache("aa-valid")
         test_set = self.model.preprocess(test_set)
 
