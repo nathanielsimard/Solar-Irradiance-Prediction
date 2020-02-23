Fix normalization

src/model/clearsky1.py
@@ -23,9 +23,9 @@ class Clearsky(base.Model):
     def __init__(self, encoder: autoencoder.Encoder):
         """Initialize the architecture."""
         super().__init__(NAME)
-        self.scaling_image = preprocessing.MinMaxScaling(
-            preprocessing.IMAGE_MIN, preprocessing.IMAGE_MAX
-        )
+        self.scaling_image = preprocessing.min_max_scaling_images()
+        self.scaling_ghi = preprocessing.min_max_scaling_ghi()
+
         self.encoder = encoder
 
         self.flatten = Flatten()
@@ -82,7 +82,11 @@ class Clearsky(base.Model):
             return self.flatten(image_encoded)[0, :]
 
         def preprocess(image, clearsky, target_ghi):
+            # Normalize inputs
             image = self.scaling_image.normalize(image)
+            clearsky = self.scaling_ghi.normalize(clearsky)
+            target_ghi = self.scaling_ghi.normalize(target_ghi)
+
             image_features = tf.py_function(func=encoder, inp=[image], Tout=tf.float32)
             clearsky = self._preprocess_target(clearsky)
             target_ghi = self._preprocess_target(target_ghi)

src/model/embed_conv3d.py
@@ -22,9 +22,8 @@ class Conv3D(base.Model):
         self.num_images = num_images
         self.time_interval_min = time_interval_min
 
-        self.scaling_image = preprocessing.MinMaxScaling(
-            preprocessing.IMAGE_MIN, preprocessing.IMAGE_MAX
-        )
+        self.scaling_image = preprocessing.min_max_scaling_images()
+        self.scaling_ghi = preprocessing.min_max_scaling_ghi()
 
         self.encoder = encoder
         self.flatten = layers.Flatten()
@@ -114,6 +113,8 @@ class Conv3D(base.Model):
 
         def preprocess(images, target_csm, target_ghi):
             images = self.scaling_image.normalize(images)
+            target_csm = self.scaling_ghi.normalize(target_csm)
+            target_ghi = self.scaling_ghi.normalize(target_ghi)
             # Warp the encoder preprocessing in a py function
             # because its size is not known at compile time.
             images = tf.py_function(func=encoder, inp=[images], Tout=tf.float32)