Fix/normalization (#86)
* Fix normalization

* Test with conv3d embed

* Cleanup

* More cleanup

* Fix wrong feature name

src/model/conv2d.py
@@ -1,7 +1,8 @@
 from typing import Tuple
 
 import tensorflow as tf
-from tensorflow.keras.layers import Conv2D, Dense, Flatten, MaxPooling2D, Dropout
+from tensorflow.keras.layers import (Conv2D, Dense, Dropout, Flatten,
+                                     MaxPooling2D)
 from tensorflow.keras.models import Sequential
 
 from src import logging
@@ -20,9 +21,8 @@ class CNN2D(base.Model):
     def __init__(self):
         """Initialize the architecture."""
         super().__init__(NAME)
-        self.scaling_image = preprocessing.MinMaxScaling(
-            preprocessing.IMAGE_MIN, preprocessing.IMAGE_MAX
-        )
+        self.scaling_image = preprocessing.min_max_scaling_images()
+        self.scaling_ghi = preprocessing.min_max_scaling_ghi()
 
         self.conv1 = self._convolution_step((5, 5), 64)
         self.conv2 = self._convolution_step((3, 3), 128)
@@ -167,12 +167,12 @@ class CNN2DClearsky(base.Model):
 
     def preprocess(self, dataset: tf.data.Dataset) -> tf.data.Dataset:
         """Applies the preprocessing to the inputs and the targets."""
-        return dataset.map(
-            lambda target_ghi_dummy, metadata, image, target_ghi: (
-                target_ghi_dummy,
-                metadata,
-                self.scaling_image.normalize(image),
-                target_ghi,
-            ),
-            num_parallel_calls=tf.data.experimental.AUTOTUNE,
-        )
+
+        def preprocess(target_ghi_dummy, metadata, image, target_ghi):
+            image = self.scaling_image.normalize(image)
+            metadata = self.scaling_ghi.normalize(metadata)
+            target_ghi = self.scaling_ghi.normalize(target_ghi)
+
+            return target_ghi_dummy, metadata, image, target_ghi
+
+        return dataset.map(preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE)

src/model/conv3d.py
@@ -20,10 +20,10 @@ class CNN3D(base.Model):
     def __init__(self, num_images=8):
         """Initialize the architecture."""
         super().__init__(NAME)
-        self.scaling_image = preprocessing.MinMaxScaling(
-            preprocessing.IMAGE_MIN, preprocessing.IMAGE_MAX
-        )
         self.num_images = num_images
+
+        self.scaling_image = preprocessing.min_max_scaling_images()
+        self.scaling_ghi = preprocessing.min_max_scaling_ghi()
 
         self.conv1 = self._convolution_step((1, 5, 5), 64)
         self.conv2 = self._convolution_step((1, 3, 3), 128)
@@ -82,6 +82,10 @@ class CNN3D(base.Model):
 
     def preprocess(self, dataset: tf.data.Dataset) -> tf.data.Dataset:
         """Applies the preprocessing to the inputs and the targets."""
-        return dataset.map(
-            lambda image, target_ghi: (self.scaling_image.normalize(image), target_ghi,)
-        )
+
+        def preprocess(images, target_ghi):
+            images = self.scaling_image.normalize(images)
+            target_ghi = self.scaling_ghi.normalize(target_ghi)
+            return images, target_ghi
+
+        return dataset.map(preprocess)

src/model/gru.py
@@ -22,13 +22,8 @@ class GRU(base.Model):
         self.num_images = num_images
         self.time_interval_min = time_interval_min
 
-        self.scaling_image = preprocessing.MinMaxScaling(
-            preprocessing.IMAGE_MIN, preprocessing.IMAGE_MAX
-        )
-
-        self.scaling_target = preprocessing.MinMaxScaling(
-            preprocessing.TARGET_GHI_MIN, preprocessing.TARGET_GHI_MAX
-        )
+        self.scaling_image = preprocessing.min_max_scaling_images()
+        self.scaling_ghi = preprocessing.min_max_scaling_ghi()
 
         self.encoder = encoder
         self.flatten = layers.Flatten()
@@ -97,6 +92,8 @@ class GRU(base.Model):
 
         def preprocess(images, clearsky, target_ghi):
             images = self.scaling_image.normalize(images)
+            clearsky = self.scaling_ghi.normalize(clearsky)
+            target_ghi = self.scaling_ghi.normalize(target_ghi)
             # Warp the encoder preprocessing in a py function
             # because its size is not known at compile time.
             features = tf.py_function(

src/training.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import tensorflow as tf
 
 from src import env, logging
+from src.data import preprocessing
 from src.data.train import load_data
 from src.model.base import Model
 
@@ -58,12 +59,18 @@ class Training(object):
     """
 
     def __init__(
-        self, optimizer: tf.keras.optimizers, model: Model, loss_fn: tf.keras.losses,
+        self,
+        optimizer: tf.keras.optimizers,
+        model: Model,
+        loss_fn: tf.keras.losses,
+        predict_ghi=True,
     ):
         """Initialize a training session."""
         self.optim = optimizer
         self.loss_fn = loss_fn
         self.model = model
+        self.predict_ghi = predict_ghi
+        self.scaling_ghi = preprocessing.min_max_scaling_ghi()
 
         self.metrics = {
             "train": tf.keras.metrics.Mean("train loss", dtype=tf.float32),
@@ -166,12 +173,16 @@ class Training(object):
         writer = self.writer[name]
 
         for i, data in enumerate(dataset.batch(batch_size)):
-            logger.info(f"Evaluation batch #{i}")
+            logger.info(f"Evaluation batch #{i+1}")
             inputs = data[:-1]
             targets = data[-1]
 
             loss = self._calculate_loss(inputs, targets)
-            metric(loss)
+            if self.predict_ghi:
+                metric(self.scaling_ghi.original(loss))
+            else:
+                metric(loss)
+
             if dry_run:
                 break
 
@@ -188,7 +199,10 @@ class Training(object):
         gradients = tape.gradient(loss, self.model.trainable_variables)
         self.optim.apply_gradients(zip(gradients, self.model.trainable_variables))
 
-        self.metrics["train"](loss)
+        if self.predict_ghi:
+            self.metrics["train"](self.scaling_ghi.original(loss))
+        else:
+            self.metrics["train"](loss)
 
     @tf.function
     def _calculate_loss(self, valid_inputs, valid_targets):